aa6166b4f7acfdffa4df545be36ba3bf2201daf2
Luc Patiny <luc@patiny.com>
2023-09-07 07:02:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# derived-props
+
+[![NPM version][npm-image]][npm-url]
+[![build status][ci-image]][ci-url]
+[![Test coverage][codecov-image]][codecov-url]
+[![npm download][download-image]][download-url]
+
+Calculate derived properties.
+
+## Installation
+
+`$ npm i derived-props`
+
+## Usage
+
+```js
+import { myModule } from 'derived-props';
+
+const result = myModule(args);
+// result is ...
+```
+
+## License
+
+[MIT](./LICENSE)
+
+[npm-image]: https://img.shields.io/npm/v/derived-props.svg
+[npm-url]: https://www.npmjs.com/package/derived-props
+[ci-image]: https://github.com/cheminfo/derived-props/workflows/Node.js%20CI/badge.svg?branch=main
+[ci-url]: https://github.com/cheminfo/derived-props/actions?query=workflow%3A%22Node.js+CI%22
+[codecov-image]: https://img.shields.io/codecov/c/github/cheminfo/derived-props.svg
+[codecov-url]: https://codecov.io/gh/cheminfo/derived-props
+[download-image]: https://img.shields.io/npm/dm/derived-props.svg
+[download-url]: https://www.npmjs.com/package/derived-props
---
+++ src/__tests__/appendDerivedProperties.test.ts
@@ -0,0 +1,117 @@
+import { produce } from 'immer';
+import { test, expect } from 'vitest';
+
+import { appendDerivedProperties } from '../appendDerivedProperties';
+
+test('deriveProps simple', () => {
+  const data = {
+    name: 'John',
+    originalX: 30,
+    data: {
+      originalX: [1, 2, 3],
+      y: [2, 3, 4],
+    },
+    ranges: [
+      {
+        originalFrom: 1,
+        originalTo: 2,
+      },
+      {
+        originalFrom: 3,
+        originalTo: 4,
+      },
+    ],
+  };
+
+  const shift = 2;
+  const callback = (value: number) => value + shift;
+
+  // In this mapping we ignore the arrays indices
+  const appenders = {
+    'ranges.originalFrom': {
+      name: 'from',
+      callback,
+    },
+    'ranges.originalTo': {
+      name: 'to',
+      callback,
+    },
+    'data.originalX': {
+      name: 'x',
+      callback,
+    },
+    originalX: {
+      name: 'x',
+      callback,
+    },
+  };
+
+  appendDerivedProperties(data, appenders);
+
+  expect(data).toStrictEqual({
+    name: 'John',
+    originalX: 30,
+    data: { originalX: [1, 2, 3], y: [2, 3, 4], x: [3, 4, 5] },
+    ranges: [
+      { originalFrom: 1, originalTo: 2, from: 3, to: 4 },
+      { originalFrom: 3, originalTo: 4, from: 5, to: 6 },
+    ],
+    x: 32,
+  });
+});
+
+test('deriveProps big', () => {
+  const data = {
+    x: new Float64Array(1e7),
+  };
+
+  const start = Date.now();
+
+  const callback = (value: number) => value + 1;
+  // In this mapping we ignore the arrays indices
+  const appenders = {
+    x: {
+      name: 'y',
+      callback,
+    },
+  };
+
+  appendDerivedProperties(data, appenders);
+
+  let sum = 0;
+  for (let i = 0; i < data.x.length; i++) {
+    sum = sum + data.x[i] + data.y[i];
+  }
+  expect(sum).toBe(1e7);
+
+  expect(Date.now() - start).toBeLessThan(1000);
+});
+
+test('with immer', () => {
+  const data = {
+    x: new Float64Array(1e7),
+  };
+
+  const start = Date.now();
+
+  const callback = (value: number) => value + 1;
+  // In this mapping we ignore the arrays indices
+  const appenders = {
+    x: {
+      name: 'y',
+      callback,
+    },
+  };
+
+  const nextData = produce(data, (draft) => {
+    appendDerivedProperties(draft, appenders);
+  });
+
+  let sum = 0;
+  for (let i = 0; i < nextData.x.length; i++) {
+    sum = sum + nextData.x[i] + nextData.y[i];
+  }
+  expect(sum).toBe(1e7);
+
+  expect(Date.now() - start).toBeLessThan(1000);
+});
